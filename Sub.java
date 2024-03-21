0d68195aedc3b15501b06a08c34dd4cf6639673f
Harsha <harsha.koulali@gmail.com>
2024-03-21 14:06:53 +0530

    comitting Sub.java

---
+++ Sub.java
@@ -0,0 +1,12 @@
+package Programs;
+
+public class Sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=1,b=1,c;
+		
+		System.out.println(c=a-b);
+	}
+
+}
