09d7a885a3d4297656a9a8ebf7d48d41bed334ca
harsha-k-code <78655315+harsha-k-code@users.noreply.github.com>
2024-03-21 13:42:19 +0530

    first commit

---
+++ Add.java
@@ -0,0 +1,13 @@
+package Programs;
+
+public class Add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=1,b=1,c;
+		
+		System.out.println(c=a+b);
+	}
+
+}
